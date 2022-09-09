26d17b347220e2541e3703f595cd66d86b9bbeae
AHEYOND <aheyond@gmail.com>
2022-09-09 08:44:44 -0500

    Programa que muestra tipos de datos

---
+++ TiposDeDatos.java
@@ -0,0 +1,17 @@
+package tipos.de.datos;
+//estilo de programacion K&R
+//@author Luis
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        //se muestra en consola los distintos tipos de datos de java
+        System.out.println("string: 'hola'"
+                + "\nboolean: true / false"
+                + "\nchar: 'a' 'A' '6'"
+                + "\nbyte: de –128 a 127"
+                + "\nshort: de –32,768 a 32,767"
+                + "\nint: de –2,147,483,648 a 2,147,483,647"
+                + "\nlong: de 9,223,372,036,854,775,808 a 9,223,372,036,854,775,807"
+                + "\ndouble: de 4.9e–324 a 1.8e+308"
+                + "\nfloat: de 1.4e–045 a 3.4e+038");
+    }
+}
